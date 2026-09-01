7fc405abafd86c870768ba77015ed7092f9fbf7c
Raja Salman Nadeem <rajasalman044164@gmail.com>
2026-09-01 05:43:55 +0500

    Fix static collection with execution permissions

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Add user bin to PATH
-export PATH=$PATH:/vercel/.local/bin
-
 # Install requirements
 python3 -m pip install --break-system-packages --user -r requirements.txt
 
-# Collect static files
-python3 manage.py collectstatic --no-input
+# Collect static files explicitly
+python3 manage.py collectstatic --no-input --clear
