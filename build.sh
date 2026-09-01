1d38838e0a1ebe6301b38d771ce8ceeae78a8dc2
Raja Salman Nadeem <rajasalman044164@gmail.com>
2026-09-01 05:46:47 +0500

    Fix CDN static routing output

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install requirements
 python3 -m pip install --break-system-packages --user -r requirements.txt
-
-# Collect static files explicitly
 python3 manage.py collectstatic --no-input --clear
