4cbdbf8bcd4c15e14853e81ea2242bcfc3b4c713
Raja Salman Nadeem <rajasalman044164@gmail.com>
2026-09-01 06:05:22 +0500

    Add gunicorn and update config for Render

---
+++ api/index.py
@@ -0,0 +1,4 @@
+from ayan_portfolio.wsgi import application
+
+# Vercel serverless function entrypoint
+app = application
--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-# Install dependencies in standard global environment inside Vercel container
 pip install -r requirements.txt
 
-# Run collectstatic to generate staticfiles folder
-python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input
+python manage.py migrate
