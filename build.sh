1e89c4291a3c92fd990a0a8a32440bb648fae218
Raja Salman Nadeem <rajasalman044164@gmail.com>
2026-09-01 05:09:40 +0500

    Fix PEP 668 pip install error for Vercel build

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
+# Install dependencies using --break-system-packages flag for Vercel's Python environment
+python3 -m pip install --break-system-packages -r requirements.txt
+
+# Collect static files
+python3 manage.py collectstatic --no-input
